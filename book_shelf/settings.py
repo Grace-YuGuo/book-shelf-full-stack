38f4fcf28d33b4b010be603ed22188ffb615c977
Yu Guo <grace.guo012@gmail.com>
2024-07-29 16:40:11 +0000

    Install packages to connect postgresql database and set up database connection related in settings.py

--- book_shelf/settings.py
+++ book_shelf/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%(sj25mdyz(lh8xya=21s(7p3buf(_o$^9a6fs6w=e2$uao1y1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['8000-graceyuguo-bookshelf-qiz2udug1ky.ws.codeinstitute-ide.net','.herokuapp.com']
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-graceyuguo-bookshelf-qiz2udug1ky.ws.codeinstitute-ide.net",
+]
 
 # Application definition
 
@@ -74,13 +81,17 @@ WSGI_APPLICATION = 'book_shelf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# Connect postgreSql database by parsing the variable of DATABASES_URL
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
