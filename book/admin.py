ab69162e42ef37d12536a061c05672d4b6810cdb
Yu Guo <grace.guo012@gmail.com>
2024-07-31 19:50:37 +0000

    Install summernote app and set it up in related files

--- book/admin.py
+++ book/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Book,Category,Review
 # Register your models here.
+# Define a class inheritated from SummernoteModelAdmin to customise the book model layout in admin
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+    list_display=('title','author','category','created_on')
+    search_fields=['title','author']
+    list_filter = ('pages',)
+    summernote_fields = ('description',)
 
-admin.site.register(Book)
+# admin.site.register(Book)
 admin.site.register(Category)
 admin.site.register(Review)
