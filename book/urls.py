5da836f540671856025c61b448e3e00363c56661
Yu Guo <grace.guo012@gmail.com>
2024-08-01 17:45:48 +0000

    Click book title to view the book detail

--- book/urls.py
+++ book/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # Set up urlpatterns for book app
 urlpatterns = [
-    path('', views.BookList.as_view(), name='book'),
+    path('', views.BookList.as_view(), name='home'),
+    path('view/<book_id>',views.book_detail, name='book_detail'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 # from django.views.generic import TemplateView
 # from django.http import HttpResponse
 from django.views import generic
@@ -16,6 +16,27 @@ from .models import Book
 
 # Set up a generic view inherites from generic.ListView class to display all the books
 class BookList(generic.ListView):
-    queryset= Book.objects.all()
+    queryset= Book.objects.filter(approved=1)
     template_name = "book/index.html"
     paginate_by = 4
+
+def book_detail(request, book_id):
+    """
+    Display an individual : model:`book.Book`.
+    
+    **Context**
+    ``book``
+        An instance of : model:`book.Book`.
+    ``reviews``
+        All approved reviews related to the book.
+    ``reviews_count``
+        A count of approved reviews related to the book.
+    ``review_form``
+        An instance of :form:`book.ReviewsForm`.
+
+    **Template:**
+    :template:`book/book_detail.html`
+    """
+    retrieved_book = get_object_or_404(Book, id=book_id)
+    return render(request,"book/book_detail.html",{"book": retrieved_book,},)
+
