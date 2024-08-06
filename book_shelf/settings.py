b030661a40292010df8f69d7072bd8dd92001980
Yu Guo <grace.guo012@gmail.com>
2024-08-06 08:41:38 +0000

    Add default bookcover image in index.html

--- book_shelf/settings.py
+++ book_shelf/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -58,7 +59,6 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
-    'cloudinary_storage',
     'cloudinary',
     'book',
 ]
